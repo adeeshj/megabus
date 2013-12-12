b205ad411d041bcefe403a22717ec98984f8b289
adeeshj <adeeshj@gmail.com>
2013-12-12 10:29:34 -0600

    Added pull of depart, arrive, and cost info

--- megabus.py
+++ megabus.py
@@ -1,8 +1,11 @@
 import urllib
+import webbrowser
 from bs4 import BeautifulSoup
-print 'MegaBus Ticket Checker v0.1\n'
+print 'MegaBus Ticket Checker v0.2\n'
 
-data =  urllib.urlopen('http://us.megabus.com/JourneyResults.aspx?originCode=317&destinationCode=320&outboundDepartureDate=1%2f12%2f2014&inboundDepartureDate=&passengerCount=1&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=&withReturn=0')
+
+url = 'http://us.megabus.com/JourneyResults.aspx?originCode=317&destinationCode=320&outboundDepartureDate=1%2f12%2f2014&inboundDepartureDate=&passengerCount=1&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=&withReturn=0'
+data =  urllib.urlopen(url)
 #Using the date Jan 12, 2014 from Dallas to Austin.
 
 webData = data.read()
@@ -19,9 +22,26 @@ data = []
 
 for tag in dayData:
 	tempdata = []
-	tempstring = ""
+	departure = ""
 	for string in tag.find("li", class_="two").p.stripped_strings:
-		tempstring = tempstring + " " + string
-	tempdata.append(tempstring.encode('utf-8'))
-	
+		departure = departure + " " + string
+	tempdata.append(departure[1:].encode('utf-8'))
+	arrive = ""
+	for string in tag.find("li", class_="two").find("p", class_="arrive").stripped_strings:
+		arrive = arrive + " " + string
+	tempdata.append(arrive[1:].encode('utf-8'))
+	for string in tag.find("li", class_="five").p.stripped_strings:
+		cost = string
+	tempdata.append(cost.encode('utf-8'))	
+	data.append(tempdata)
+
+print 'The current trip details are:\n'
+
+for each_trip in data:
+	print each_trip[0]
+	print each_trip[1]
+	print "Cost: " + each_trip[2] + '\n'
+
+webbrowser.open_new_tab(url)
+
 	
