3f53036ecc1fbfe59fe37c7543a44e97b54b2f1e
adeeshj <adeeshj@gmail.com>
2013-12-18 22:10:02 -0600

    Added ability for script to send emails

--- megabus.py
+++ megabus.py
@@ -1,12 +1,41 @@
 import urllib
 import webbrowser
+import smtplib
 from bs4 import BeautifulSoup
-print 'MegaBus Ticket Checker v0.5\n'
+print 'MegaBus Ticket Checker v0.8\n'
+
+mailSender = "" #Gmail account being used to send email
+mailSenderPassword = "" #Gmail account's password
+mailReceiver = "" #Recipient of email
 
 
-url = 'http://us.megabus.com/JourneyResults.aspx?originCode=317&destinationCode=320&outboundDepartureDate=1%2f12%2f2014&inboundDepartureDate=&passengerCount=1&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=&withReturn=0'
+def url_customization(): #returns string of url
+	departureDateMonth = input("Enter the departure month (as a number): ")
+	departureDateDay = input("Enter the departure day: ")
+	departureDayYear = input("Enter the departure year: ")
+	customURL = 'http://us.megabus.com/JourneyResults.aspx?originCode=317&destinationCode=320&outboundDepartureDate={}%2f{}%2f{}&inboundDepartureDate=&passengerCount=1&transportType=0&concessionCount=0&nusCount=0&outboundWheelchairSeated=0&outboundOtherDisabilityCount=0&inboundWheelchairSeated=0&inboundOtherDisabilityCount=0&outboundPcaCount=0&inboundPcaCount=0&promotionCode=&withReturn=0' .format(departureDateMonth, departureDateDay, departureDayYear)
+	return customURL
+
+def send_email(data):
+	gmailserver = smtplib.SMTP("smtp.gmail.com", 587)
+	gmailserver.ehlo()
+	gmailserver.starttls()
+	gmailserver.ehlo()
+	gmailserver.login(mailSender, mailSenderPassword)
+
+	header = "Megabus Trip Details"
+	message = ""
+
+	for each_trip in data:
+		message = message + each_trip[0] + "\n" + each_trip[1] + "\n" + each_trip[2] + "\n\n"
+
+	gmailserver.sendmail(mailSender, mailReceiver, header + "\n\n\n" + message)
+
+	gmailserver.quit()
+
+url = url_customization()
+
 data =  urllib.urlopen(url)
-#Using the date Jan 12, 2014 from Dallas to Austin.
 
 webData = data.read()
 soupData = BeautifulSoup(webData)
@@ -16,7 +45,7 @@ if 'No journeys have been found for the date selected' in webData:
 else:
 	print "Buy your Megabus tickets!"
 
-dayData = soupData.body.form.find("div", id="wrapper").find("div", id="container").find("div", class_="mainContent").find("div", class_="cont_wraper").find("div", class_="cont_middlecurve").div.div.div.div.div.find("div", class_="journeyresult").find("div", class_="content").div.find_all("ul", class_="journey standard")
+dayData = soupData.find_all("ul", class_="journey standard")
 
 data = []
 
@@ -35,12 +64,13 @@ for tag in dayData:
 	tempdata.append(cost.encode('utf-8'))	
 	data.append(tempdata)
 
-print 'The current trip details are:\n'
+#print 'The current trip details are:\n'
+#
+#for each_trip in data:
+#	print each_trip[0]
+#	print each_trip[1]
+#	print "Cost: " + each_trip[2] + '\n'
 
-for each_trip in data:
-	print each_trip[0]
-	print each_trip[1]
-	print "Cost: " + each_trip[2] + '\n'
+send_email(data)
 
-
-	
+print "done"
